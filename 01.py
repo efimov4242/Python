0fab7a230bcd7139d3f848bb24bf92d491f2d7fb
Ilya <efimov4242@gmail.com>
2022-12-14 10:06:12 +0400

    Написал решение с помощью функции

--- 01.py
+++ 01.py
@@ -6,12 +6,17 @@
 # - 7 -> да
 # - 1 -> нет
 
-print("Введите день недели: ")
-week_day = int(input())
-while(week_day <= 0 or week_day > 7):
-	print("Введите число от 0 до 7: ")
-	week_day = int(input())
-if week_day > 0 and week_day <= 5:
-	print("Нет")
-if week_day > 5 and week_day <= 7:
-	print("Да")
+def isWeekendDay():
+	print("Введите день недели: ")
+	dayNumber = int(input())
+
+	while(dayNumber <= 0 or dayNumber > 7):
+		print("Введите число от 0 до 7: ")
+		dayNumber = int(input())
+
+	if dayNumber > 0 and dayNumber <= 5:
+		print ("Нет")
+	if dayNumber > 5 and dayNumber <= 7:
+		print ("Да")
+
+isWeekendDay()
