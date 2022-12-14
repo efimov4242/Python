027eb91ae73cc295eb01f6bde8c88833f2c49db1
Ilya <efimov4242@gmail.com>
2022-12-14 09:28:48 +0400

    Добавил цикл while

--- 01.py
+++ 01.py
@@ -8,9 +8,9 @@
 
 print("Введите день недели: ")
 week_day = int(input())
-
-if week_day <= 0 or week_day > 7:
-	print("Введите число от 0 до 7")
+while(week_day <= 0 or week_day > 7):
+	print("Введите число от 0 до 7: ")
+	week_day = int(input())
 if week_day > 0 and week_day <= 5:
 	print("Нет")
 if week_day > 5 and week_day <= 7:
